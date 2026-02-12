e4d7ad00e9e3defe13f7743e6aaeeca39a4d8e1c
Amit kushwaha <amit01golu@gmail.com>
2026-02-12 08:42:22 +0530

    Create OOP.py

---
+++ OOP.py
@@ -0,0 +1,11 @@
+class Employee:
+    Name ="AMIT"
+    Address = "Indore"
+    Year = 2026
+    Salary = 6000000  
+
+    def print(self):
+      print("Value si .............")
+    
+Am = Employee()
+print(Am.Name  , Am.Salary)
