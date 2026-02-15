32aea255ce3307c8f61225fdb495e44b50ef7a4d
Amit kushwaha <amit01golu@gmail.com>
2026-02-15 20:54:10 +0530

    Fix typo in multi-line print statement

--- print.py
+++ print.py
@@ -1,6 +1,9 @@
+# print one line  code 
+
 print("Hello : Python")
 
+# print multiple line code
 print(''' hi this is AMIT
 python Multiple line print 
-this is anither line
+this is anither line ...../
 ''')
