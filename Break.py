ccd4f62b489dd7569fbcab734c7db61d82e04e2d
Amit kushwaha <amit01golu@gmail.com>
2026-02-18 08:12:40 +0530

    Refactor Break.py with continue example
    Reformatted Break.py and added a continue example.

--- Break.py
+++ Break.py
@@ -1,4 +1,12 @@
+#bREAK
 for i in range(100):
     if(i==41):
         break  # Exit the loop right now
     print(i)    
+
+# continue
+for i in range (100):
+    if(i==33):
+        continue
+    print(i)    
+
