c75203db36ed25ac0b63143ae59c0fb152b28a40
Amit kushwaha <amit01golu@gmail.com>
2026-02-19 20:10:50 +0530

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,14 +1,18 @@
-# for loop
-# n = int (input("Enter anny number"))
-# for i in range(1,n):
-#     print(i)
+# Print("LOOP .............................")
 
-# i=1
-# while (i<22):
-#     print(i)
-#     i+=1
+for loop
+n = int (input("Enter anny number"))
+for i in range(1,n):
+    print(i)
+
+i=1
+while (i<22):
+    print(i)
+    i+=1
 
 
-# n= int(input("Enter any no:"))
-# for i in range (0,1000,n):
-#     print(i)
+n= int(input("Enter any no:"))
+for i in range (0,1000,n):
+
+    print(i)
+
