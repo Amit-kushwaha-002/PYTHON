93ceac2ed59dfd49927c6e77a51b721955207a38
Amit kushwaha <amit01golu@gmail.com>
2026-02-19 20:17:28 +0530

    Update sum.py

--- sum.py
+++ sum.py
@@ -1,6 +1,6 @@
-a=int(input("Enter any number"))
-b=int(input("Enter any number"))
-print("A+B =",a+b)
+a=int(input("Enter any number"))  # input 1st number 
+b=int(input("Enter any number"))  # input 2nd number 
+print("A+B =",a+b)   
 for i in range 9{
   print(a);
   print(b);
