23892e97ec8b1d1600dd46b282770bdf3666ea3b
Amit kushwaha <amit01golu@gmail.com>
2026-02-26 12:03:08 +0530

    Create reverse.py

---
+++ reverse.py
@@ -0,0 +1,7 @@
+arr = [45, 54, 76, 123, 25, 123] 
+max_value = max(arr) # finds the maximum value in one line
+print(f"The Maximum element in the array is: {max_value}")
+
+# Reversing a list is also very simple
+reversed_arr = arr[::-1]
+print(f"The reversed array is: {reversed_arr}")
