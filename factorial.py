8ba21275c69ca1ea5c10072bad107cf8ed4275b6
Amit kushwaha <amit01golu@gmail.com>
2026-05-06 07:29:40 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,3 +1,11 @@
+# def factorial(n):
+#     if(n==1 or n==0):
+#         return 1
+#     return n*factorial(n-1)
+
+# n=int(input("Enter any numbert :")) 
+# print(factorial(n))
+
 def factorial(n):
     if(n==1 or n==0):
         return 1
